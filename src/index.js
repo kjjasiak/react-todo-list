65f9f3a25600b13e66549dbaf9e342276c65b105
Karolina Jasiak <kjasiak1993@gmail.com>
2020-11-27 20:16:04 +0100

    Added Sidebar and AppBar components (empty for now), implemented functions and components to add new todo item

--- src/index.js
+++ src/index.js
@@ -1,69 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDrawer} from '@material/drawer';
+
+function AppBar(props) {
+  return (
+    <header className="mdc-top-app-bar app-bar" id="app-bar">
+      <div className="mdc-top-app-bar__row">
+        <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <button className="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
+          <span className="mdc-top-app-bar__title">Todo List</span>
+        </section>
+      </div>
+    </header>
+  );
+}
 
 class Sidebar extends React.Component {
   render() {
     return (
-      <div>Sidebar
-         <button className="mdc-button">
-   <div className="mdc-button__ripple"></div>
-   <span className="mdc-button__label">Text Button</span>
-</button>
-      </div>
-    );
-  }
-}
-
-class Item extends React.Component {
-  handleCheckboxChange() {
-    const currentState = this.props.state;
-    currentState.done = !currentState.done;
-    this.props.handler(currentState);
-  }
-
-  renderCheckbox() {
-    return (
-      <Checkbox
-        done={this.props.state.done}
-        onChange={() => this.handleCheckboxChange()}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <li className="todo-item">
-        {this.renderCheckbox()}
-        {this.props.state.text}
-      </li>
-    );
-  }
-}
-
-function Checkbox(props) {
-  return (
-    <div className="mdc-form-field">
-      <div className="mdc-checkbox">
-        <input type="checkbox"
-              className="mdc-checkbox__native-control"
-              id="checkbox-1"
-              onChange={() => props.onChange()} checked={props.done}/>
-        <div className="mdc-checkbox__background">
-          <svg className="mdc-checkbox__checkmark"
-              viewBox="0 0 24 24">
-            <path className="mdc-checkbox__checkmark-path"
-                  fill="none"
-                  d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
-          </svg>
-          <div className="mdc-checkbox__mixedmark"></div>
+      <aside className="mdc-drawer mdc-drawer--dismissible mdc-top-app-bar--fixed-adjust">
+        <div className="mdc-drawer__content">
+          <div className="mdc-list todo-labels-list">
+          </div>
         </div>
-        <div className="mdc-checkbox__ripple"></div>
-      </div>
-      {/* <label for="checkbox-1">Checkbox 1</label> */}
-    </div>
-    // <input type="checkbox" onChange={() => props.onChange()} checked={props.done} />
-  );
+      </aside>
+    );
+  }
 }
 
 class TodoList extends React.Component {
@@ -74,8 +38,12 @@ class TodoList extends React.Component {
       items: [],
     };
 
-    this.handler = this.handler.bind(this);
+    this.removeHandler = this.removeHandler.bind(this);
+    this.changeHandler = this.changeHandler.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
 
+  componentDidMount() {
     const sampleItems = [
       {
         id: "aiirx7-qutypr",
@@ -104,15 +72,24 @@ class TodoList extends React.Component {
 
     if (savedItems == null) {
       localStorage.setItem('react-todo-list', JSON.stringify(sampleItems));
-      this.state.items = sampleItems;
+      this.setState({
+        items: sampleItems
+      });
     }
     else {
-      this.state.items = JSON.parse(localStorage.getItem('react-todo-list'));
+      this.setState({
+        items: JSON.parse(localStorage.getItem('react-todo-list'))
+      });
     }
-    console.log(this.state.items);
   }
 
-  handler(itemState) {
+  removeHandler(itemId) {
+    this.setState({
+      items: this.state.items.filter(item => item.id !== itemId)
+    });
+  }
+
+  changeHandler(itemState) {
     const itemsModified = this.state.items.map((item, index) => {
       if (item.id === itemState.id) {
         item = itemState;
@@ -132,8 +109,24 @@ class TodoList extends React.Component {
     return Math.random().toString(36).slice(-6) + '-' + Math.random().toString(36).slice(-6);
   }
 
+  addItem() {
+    const newItem = {
+      id: this.generateItemUid(),
+      text: '',
+      date: null,
+      done: false,
+      labels: []
+    };
+
+    this.setState({
+      items: this.state.items.concat(newItem)
+    });
+
+    console.log(this.state.items);
+  }
+
   renderItem(id, state) {
-    return <Item key={id} handler={this.handler} state={state} />;
+    return <Item key={id} removeHandler={this.removeHandler} changeHandler={this.changeHandler} state={state} />;
   }
 
   render() {
@@ -144,22 +137,160 @@ class TodoList extends React.Component {
     });
 
     return (
-      <ul>
-        {itemsRender}
-      </ul>
+      <div>
+        <ul id="todo-list" className="mdc-list" role="group" aria-label="">
+          {itemsRender}
+        </ul>
+        <AddItemButton onClick={this.addItem}/>
+      </div>
+    );
+  }
+}
+
+function AddItemButton(props) {
+  return (
+    <button onClick={props.onClick}>Add</button>
+  );
+}
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput = React.createRef();
+
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    this.handleTextInputChange = this.handleTextInputChange.bind(this);
+    this.removeIfEmpty = this.removeIfEmpty.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.textInput.current.isTextInputEmpty()) {
+      this.textInput.current.focusTextInput();
+    }
+  }
+
+  handleCheckboxChange() {
+    const currentState = this.props.state;
+    currentState.done = !currentState.done;
+    this.props.changeHandler(currentState);
+  }
+
+  handleTextInputChange(e) {
+    const currentState = this.props.state;
+    currentState.text = e.target.value;
+    this.props.changeHandler(currentState);
+  }
+
+  removeItem(id) {
+    this.props.removeHandler(id);
+  }
+
+  removeIfEmpty(e) {
+    if (e.target.value.length < 3) {
+      this.removeItem(e.target.parentNode.parentNode.id);
+    }
+  }
+
+  renderCheckbox() {
+    return (
+      <Checkbox
+        done={this.props.state.done}
+        onChange={this.handleCheckboxChange}
+      />
+    );
+  }
+
+  renderTextInput() {
+    return (
+      <TextInput
+        ref={this.textInput}
+        text={this.props.state.text}
+        placeholder="Write your todo here (min. 3 characters)"
+        onBlur={this.removeIfEmpty}
+        onChange={this.handleTextInputChange}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <li className="mdc-list-item" id={this.props.state.id} /*role="checkbox"*/><span className="mdc-list-item__ripple"></span>
+        {this.renderCheckbox()}
+        {this.renderTextInput()}
+        <div className="mdc-chip-set mdc-chip-set--input" role="grid"></div>
+        <input type="date" placeholder="YYYY-MM-dd" className="todo-date" name="todo-date" />
+        <span className="mdc-list-item__meta">
+          <button className="mdc-icon-button material-icons todo-delete">delete</button>
+        </span>
+      </li>
+    );
+  }
+}
+
+function Checkbox(props) {
+  return (
+    <span className="mdc-list-item__graphic">
+      <div className="mdc-checkbox">
+        <input type="checkbox"
+               className="mdc-checkbox__native-control todo-status"
+               onChange={props.onChange} checked={props.done}/>
+        <div className="mdc-checkbox__background">
+          <svg className="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+            <path className="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
+          </svg>
+          <div className="mdc-checkbox__mixedmark"></div>
+        </div>
+      </div>
+    </span>
+  );
+}
+
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.textInput = React.createRef();
+  }
+
+  isTextInputEmpty() {
+    return this.textInput.current.value.length < 3;
+  }
+
+  focusTextInput() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div className="mdc-text-field text-field mdc-text-field--fullwidth mdc-text-field--no-label mdc-ripple-upgraded todo-input">
+        <input type="text" ref={this.textInput} onBlur={this.props.onBlur} onChange={this.props.onChange} placeholder={this.props.placeholder} className="mdc-text-field__input todo-text" aria-label="" value={this.props.text}/>
+      </div>
     );
   }
 }
 
 class App extends React.Component {
+  componentDidMount() {
+    const drawer = new MDCDrawer(document.querySelector('.mdc-drawer')),
+    topAppBar = new MDCTopAppBar(document.getElementById('app-bar'));
+
+    drawer.open = window.outerWidth >= 768;
+
+    topAppBar.setScrollTarget(document.getElementById('main-content'));
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+      drawer.open = !drawer.open;
+    });
+  }
   render() {
     return (
       <div className="app">
-        <div className="sidebar">
-          <Sidebar />
-        </div>
-        <div className="todo-list">
-          <TodoList />
+        <AppBar />
+        <Sidebar />
+        <div className="mdc-drawer-app-content mdc-top-app-bar--fixed-adjust">
+          <main className="main-content" id="main-content">
+            <TodoList />
+          </main>
         </div>
       </div>
     );
